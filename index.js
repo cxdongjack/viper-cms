c0f648e3f07dc9bbe380ba6785854cc9ae5e71a4
chenxiaodong <chenxiaodong@xiaomi.com>
2017-04-04 16:41:19 +0800

    opt: replace express with restify

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-var http = require('http');
-var express = require('express');
-var app = express();
-var server = http.createServer(app);
-var port = 9000;
-console.log(`服务器运行在 http://localhost:${port}/`);
-server.listen(port);
+var restify = require('restify');
+var plugins = require('restify-plugins');
 
-app.use('/', function(req, res, next) {
-    res.send(`${__dirname} OK`);
+
+const server = restify.createServer({
+    name: 'viper-cms',
+    version: '1.0.0'
+});
+server.use(plugins.acceptParser(server.acceptable));
+server.use(plugins.queryParser());
+server.use(plugins.bodyParser());
+// TODO配置logger
+server.use(restify.requestLogger());
+
+
+server.get('/scheme/:id', function(req, res, next) {
+    res.send(req.params);
+    return next();
+});
+
+server.post('/scheme', function(req, res, next) {
+    //req.log.info({body: req.body}, 'Hello there %s', 'foo');
+    console.log(req.body);
+    res.send();
+    return next();
+});
+
+server.patch('/scheme/:id', function(req, res, next) {
+    res.send(req.params);
+    return next();
+});
+
+server.del('/scheme/:id', function(req, res, next) {
+    res.send(req.params);
+    return next();
+});
+
+server.listen(8080, function() {
+    console.log('%s listening at %s', server.name, server.url);
 });
